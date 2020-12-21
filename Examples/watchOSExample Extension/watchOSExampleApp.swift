2ffc85938d0b54cb1a35c7504cd6d492e3b92054
Chris Mash <chris.mash@gmx.com>
2020-12-21 22:04:02 +0000

    Added a watchOS example

---
+++ Examples/watchOSExample Extension/watchOSExampleApp.swift
@@ -0,0 +1,19 @@
+//
+//  ExamplesApp.swift
+//  watchOSExample Extension
+//
+//  Created by Chris Mash on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
